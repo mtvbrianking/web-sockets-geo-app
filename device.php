a2ecac94b5b0c8bc7bf898420c6b7a0b314c4d40
mtvbrianking <mtvbrianking@gmail.com>
2018-12-21 13:39:07 +0300

    Setup PHP emitter

---
+++ device.php
@@ -0,0 +1,52 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+require 'device-logs.php';
+
+try {
+
+  $redis = new Predis\Client([
+    'scheme' => 'tcp',
+    'host' => '127.0.0.1',
+    'port' => 6379,
+  ]);
+
+  // print json_encode($deviceLogs);
+
+  # -----------------------------------------------------------
+
+  // Set counter.
+  // When device started reporting...
+  $start_at = strtotime($deviceLogs[0]['reported_at']);
+
+  foreach($deviceLogs as &$deviceLog) {
+    // Don't time out...
+    set_time_limit(0);
+
+    // Determine time elapsed since device started broadcasting...
+    $reported_at = strtotime($deviceLog['reported_at']);
+    // $offset = ($reported_at - $start_at); // realtime
+    $offset = round(($reported_at - $start_at)/60); // 60X faster
+    $deviceLog['offset'] = $offset;
+
+    flush();
+
+    print 'Sleep => '.$offset.'seconds'.PHP_EOL;
+
+    sleep($offset);
+
+    print date('Y-m-d H:i:s').' => '.$deviceLog['event'].PHP_EOL;
+
+    // Send to NodeJS Socket.IO
+    $redis->publish('device_channel', json_encode($deviceLog));
+
+    // restart counter...
+    $start_at = strtotime($deviceLog['reported_at']);
+  }
+
+  # -----------------------------------------------------------
+
+} catch (Exception $e) {
+  die($e->getMessage());
+}
--- index.js
+++ index.js
@@ -1,3 +1,42 @@
 require('dotenv').config();
 
-console.log(process.env.MAP_API_KEY);
+// console.log(process.env.MAP_API_KEY);
+
+var express = require('express');
+var socket = require('socket.io');
+var redis = require('redis');
+
+// Bootstrap express
+
+var app = express();
+
+var server = app.listen(4000, function(){
+	console.log('listening on *:4000');
+});
+
+// Static files
+
+app.use('/', express.static('public'));
+
+// Bootstrap socket.io
+
+var io = socket(server);
+
+io.on('connection', function(socket) {
+	
+	console.log(socket.id+' -> connected');
+
+	socket.on('disconnect', function() {
+    	console.log(socket.id+' -> disconnected');
+  	});
+});
+
+// Serverside (PHP) message...
+
+var client = redis.createClient("redis://127.0.0.1:6379");
+
+client.subscribe('device_channel');
+
+client.on("message", function(channel, data) {
+    console.log("Channel: %s -> %s", channel, data);
+});
